de6c81cc995f3e5bfe4132a62ec0a3a62fb96218
YASHAXKX <156406490+YASHAXKX@users.noreply.github.com>
2025-08-28 14:17:46 +0530

    Add files via upload

---
+++ encapsulation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class SmartMeterAccount
+{
+    private decimal balance;   
+
+    
+    public void Recharge(decimal amount)
+    {
+        balance += amount;
+        Console.WriteLine($"Balance after recharge: {balance}");
+    }
+
+    
+    public void Consume(decimal amount)
+    {
+        if (amount <= balance)
+        {
+            balance -= amount;
+            Console.WriteLine($"Balance after consumption: {balance}");
+        }
+        else
+        {
+            Console.WriteLine("Insufficient balance");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SmartMeterAccount account = new SmartMeterAccount();
+
+        
+        account.Recharge(500);
+        account.Consume(200);
+        account.Consume(400);
+    }
+}
---
+++ interinatnce.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class SmartMeter
+{
+    public int MeterId { get; set; }
+    public string CustomerName { get; set; }
+
+    public SmartMeter(int meterId, string customerName)
+    {
+        MeterId = meterId;
+        CustomerName = customerName;
+    }
+}
+
+class ResidentialMeter : SmartMeter
+{
+    public string HouseType { get; set; }
+
+    public ResidentialMeter(int meterId, string customerName, string houseType)
+        : base(meterId, customerName)
+    {
+        HouseType = houseType;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Residential Meter -> ID: {MeterId}, Name: {CustomerName}, HouseType: {HouseType}");
+    }
+}
+
+class CommercialMeter : SmartMeter
+{
+    public string BusinessType { get; set; }
+
+    public CommercialMeter(int meterId, string customerName, string businessType)
+        : base(meterId, customerName)
+    {
+        BusinessType = businessType;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Commercial Meter -> ID: {MeterId}, Name: {CustomerName}, BusinessType: {BusinessType}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ResidentialMeter rMeter = new ResidentialMeter(201, "Alice", "Apartment");
+        CommercialMeter cMeter = new CommercialMeter(301, "Bob", "Shop");
+
+        rMeter.Display();
+        cMeter.Display();
+    }
+}
+
