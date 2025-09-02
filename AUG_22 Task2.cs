26842da4bb3a85e6281855cd21a0f004b36ca018
YASHAXKX <156406490+YASHAXKX@users.noreply.github.com>
2025-09-02 16:00:54 +0530

    Add files via upload

---
+++ AUG_22 Task2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Customer
+{
+    // Properties
+    public int CustomerID { get; set; }
+    public string Name { get; set; }
+    public int UnitsConsumed { get; set; }
+
+    // Constructor
+    public Customer(int customerID, string name, int unitsConsumed)
+    {
+        CustomerID = customerID;
+        Name = name;
+        UnitsConsumed = unitsConsumed;
+    }
+
+    // Method to show bill
+    public void ShowBill()
+    {
+        int billAmount = UnitsConsumed * 5;
+        Console.WriteLine($"Customer: {Name} (ID: {CustomerID})");
+        Console.WriteLine($"Units Consumed: {UnitsConsumed}");
+        Console.WriteLine($"Total Bill: ${billAmount}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Taking input
+        Console.Write("Enter Customer ID: ");
+        int id = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter Name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter Units Consumed: ");
+        int units = Convert.ToInt32(Console.ReadLine());
+
+        // Creating object
+        Customer customer = new Customer(id, name, units);
+
+        // Displaying bill
+        customer.ShowBill();
+    }
+}
