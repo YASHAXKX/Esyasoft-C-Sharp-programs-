8146c19a55062568188bf5b351f1e9064a6a9f32
YASHAXKX <156406490+YASHAXKX@users.noreply.github.com>
2025-09-02 15:56:00 +0530

    Add files via upload

---
+++ AUG_22 Task 1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class SmartMeter
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter units consumed: ");
+        int units = Convert.ToInt32(Console.ReadLine());
+        int bill = 0;
+
+        if (units <= 100)
+        {
+            bill = units * 5;
+        }
+        else if (units <= 200)
+        {
+            bill = (100 * 5) + ((units - 100) * 7);
+        }
+        else
+        {
+            bill = (100 * 5) + (100 * 7) + ((units - 200) * 10);
+        }
+
+        Console.WriteLine($"Total electricity bill: ${bill}");
+    }
+}
